5efa6a501e08a388bf492e31d16b1f5e047de1cb
jamopopper <60857104+jamopopper@users.noreply.github.com>
2020-04-07 20:46:29 -0700

    Update robotStuff.ino

--- robotStuff.ino
+++ robotStuff.ino
@@ -25,12 +25,12 @@
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40, Wire1);
 
 //custom math
-float robotDimensions[] = {113.137, 82.25, 82.25, 82.25}; //{coxa joint to adjacent coxa joint length, coxa length, femur length, tibia length} in mm from joint center to joint center
-float legLocation[4][3] = {{ -150, 250, 0}, {150, 250, 0}, { -150, -250, 0}, {150, -250, 0}}; //{leg selected, {foot x, foot y, foot z}} in mm from robot center
-float torsoLocation[] = {0, 0, 0}; //{center x, center y, center z} in mm from center of robot on the plane passing through all joints when perpendicular
-float torsoOrientation[] = {0, 0, 0}; //{roll, pitch, yaw} in degrees of offset
+double robotDimensions[] = {113.137, 82.25, 82.25, 82.25}; //{coxa joint to adjacent coxa joint length, coxa length, femur length, tibia length} in mm from joint center to joint center
+double legLocation[4][3] = {{ -175, 175, 0}, {175, 175, 0}, { -175, -175, 0}, {175, -175, 0}}; //{leg selected, {foot x, foot y, foot z}} in mm from robot center
+double torsoLocation[] = {0, 0, 0}; //{center x, center y, center z} in mm from center of robot on the plane passing through all joints when perpendicular
+double torsoOrientation[] = {0, 0, 0}; //{roll, pitch, yaw} in degrees of offset
+double servoPositions[4][3] = {{90, 90, 90}, {90, 90, 90}, {90, 90, 90}, {90, 90, 90}}; //
 const double servoScale = double(USMAX - USMIN) / 180;
-float servoPositions[4][3] = {{90, 90, 90}, {90, 90, 90}, {90, 90, 90}, {90, 90, 90}};
 boolean servoState = false;
 boolean servoToggle = false;
 
@@ -61,20 +61,47 @@ void loop() {
 }
 void servoCalc() {
   float jointOffset[4][2] = {{-1, 1}, {1, 1}, {-1, -1}, {1, -1}};
+  // implement heading rotation using the coordinate rotation math from calc class
   for (uint_fast8_t i = 0U; i < 4U; i++) {
-    float legLength = 0;
-    float legAngle = 0;
-    float legHold[2] = {legLocation[i][0] - torsoLocation[0] - (jointOffset[i][0] * robotDimensions[0] / 2), legLocation[i][1] - torsoLocation[1] - (jointOffset[i][1] * robotDimensions[0] / 2)};
-    legHold[0] = ((legHold[0]) * (legHold[0])) + ((legHold[1]) * (legHold[1]));
-    legLength = sqrt((legHold[0]) + ((legLocation[i][2] - torsoLocation[2]) * (legLocation[i][2] - torsoLocation[2]))); //leg extention
-    legHold[1] = sqrt(legHold[0]);
-    if (legHold[0] != 0) legAngle = asin((legLocation[i][1] - torsoLocation[1] - (jointOffset[i][1] * robotDimensions[0] / 2)) / legHold[1]); //angle from perpendicular
-    else legAngle = 0;
-    Serial.print(i);
-    Serial.print(" is at ");
-    if ((i % 2)) Serial.print(90 - (legAngle * (180 / PI)));
-    else Serial.print(90 + (legAngle * (180 / PI)));
-    Serial.print("\t");
+    // variable calulations
+    double legHold[2] = {legLocation[i][0] - torsoLocation[0] - (jointOffset[i][0] * robotDimensions[0] / 2), legLocation[i][1] - torsoLocation[1] - (jointOffset[i][1] * robotDimensions[0] / 2)};
+    double legLength = sqrt(sq(legLocation[i][2] - torsoLocation[2]) + sq(sqrt(sq(legHold[0]) + sq(legHold[1])) - robotDimensions[1]));
+    double legAngle[3] = {0, 0, acos((sq(robotDimensions[2]) + sq(robotDimensions[3]) - sq(legLength))/(2 * robotDimensions[2] * robotDimensions[3]))};
+    if (legHold[0] != 0) legAngle[0] = atan(legHold[1]/legHold[0]); //angle from horizontal
+    else legAngle[0] = 90;
+    // warnings for invalid inputs
+    if (legLength > (robotDimensions[2] + robotDimensions[3])) {
+      Serial.print(i);
+      Serial.print(" is over-extended");
+      Serial.print("\t");
+      break;
+    }
+    if (legLength < abs(robotDimensions[2] - robotDimensions[3])) {
+      Serial.print(i);
+      Serial.print(" is under-extended");
+      Serial.print("\t");
+      break;
+    }
+    if ((i % 3) && (legAngle[0] < 0 || legAngle[0] > HALF_PI)) {
+      Serial.print(i);
+      Serial.print(" coxa servo can't extend that far");
+      Serial.print("\t");
+      break;
+    } else if (!(i % 3) && (legAngle[0] > 0 || legAngle[0] < -HALF_PI)) {
+      Serial.print(i);
+      Serial.print(" coxa servo can't extend that far");
+      Serial.print("\t");
+      break;
+    }
+    // setting servo angles
+    if (i % 3) servoPositions[i][0] = 45 + (legAngle[0] * (180 / PI));
+    else servoPositions[i][0] = 135 + (legAngle[0] * (180 / PI));
+    servoPositions[i][1] = (sqrt(sq(legHold[0]) + sq(legHold[1])) - robotDimensions[1]);
+    servoPositions[i][2] = atan((legLocation[i][2] - torsoLocation[2])/((180/PI) * legAngle[2]));
+    for (uint_fast8_t j = 0U; j < 3U; j++) {
+      Serial.print(servoPositions[i][j]);
+      Serial.print("\t");
+    }
   }
   Serial.println();
 }
